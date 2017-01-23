c980a16a31366e6798c4f4ec96a5cea6d412b8c1
Andrew Bennett <potatosaladx@gmail.com>
2017-01-23 13:50:30 -0700

    Initial commit.

---
+++ lib/plug_chatterbox.ex
@@ -0,0 +1,18 @@
+defmodule PlugChatterbox do
+  @moduledoc """
+  Documentation for PlugChatterbox.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugChatterbox.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_chatterbox_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugChatterboxTest do
+  use ExUnit.Case
+  doctest PlugChatterbox
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
